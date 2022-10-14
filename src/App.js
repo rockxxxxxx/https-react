0f3638788a794c799f63b9c3210ff5de68921638
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-10-14 20:56:27 +0530

    Used try catched block to handle the error and resend the request after every five second and put a cancel button to cancel the request

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React, { useState,useCallback, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [loader,setLoader] = useState(true)
-  const  fetchData=useCallback(async ()=> {
-    const response = await fetch("https://swapi.dev/api/films")
-      const data = await response.json()
-      
-        const transfornedData = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            releaseDate: movieData.release_date,
-          };
-        });
-        
-        setMovies(transfornedData);
-        setLoader(false)
-  
-  },[]);
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState(null);
+  const [timeOut, settimeout] = useState();
+  const fetchData = useCallback(async () => {
+    setLoader(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/film");
+      if (!response.ok) {
+        throw new Error("Some thing went wrong...Retrying");
+      }
+      const data = await response.json();
 
-  useEffect(()=>{
+      const transfornedData = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transfornedData);
+    } catch (error) {
+      setError(error.message);
+      const time = setTimeout(() => {
+        fetchData();
+      }, 5000);
+      settimeout(time);
+    }
+
+    setLoader(false);
+  }, []);
+
+  useEffect(() => {
     fetchData();
-  },[fetchData])
+  }, [fetchData]);
+
+  function cancelRequset(data) {
+    clearTimeout(data);
+  }
 
   return (
     <React.Fragment>
@@ -34,7 +52,15 @@ function App() {
         <button onClick={fetchData}>Fetch Movies</button>
       </section>
       <section>
-      {loader && <div className="loader"></div>}
+        {loader && <div className="loader"></div>}
+        {error && (
+          <div>
+            Something went wrong...Retrying{" "}
+            <button onClick={() => cancelRequset(timeOut)}>
+              Cancel Requset
+            </button>
+          </div>
+        )}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
